e54fd5197f4be2c6386feccc90651f342a5275fa
alex.lazich <alex.lazich>
2021-08-12 14:55:19 -0700

    setup updates

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from distutils.core import setup
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
   name = 'expensify_client',
@@ -10,8 +6,8 @@ setup(
   version = '0.0.5',
   license='MIT',
   description = 'Python wrapper for Expensify API',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
+  long_description="See GitHub",
+  long_description_content_type="text/markdown",
   author = 'Alex Lazich',
   author_email = 'lazich.alexander@gmail.com',
   url = 'https://github.com/alazich/expensify-client',
