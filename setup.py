00d77f01f6caa651898246a1127565fb93fe46e1
alex.lazich <alex.lazich>
2021-08-12 14:30:51 -0700

    update pypi readme

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'expensify_client',
   packages = ['expensify_client'],
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Python wrapper for Expensify API',
+  long_description=long_description,
   author = 'Alex Lazich',
   author_email = 'lazich.alexander@gmail.com',
   url = 'https://github.com/alazich/expensify-client',
-  download_url = 'https://github.com/alazich/expensify-client/archive/refs/tags/v0.0.3.tar.gz',
+  download_url = 'https://github.com/alazich/expensify-client/archive/refs/tags/v0.0.5.tar.gz',
   keywords = ['api', 'accounting', 'expensify'],
   install_requires=[
           'validators',
